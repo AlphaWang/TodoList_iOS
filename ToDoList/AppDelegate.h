b1a37be7a610e47cb83f1e6f8f3f3ba10899705c
Alpha Wang <awang@coupang.com>
2015-03-30 16:05:11 +0800

    [TodoList] first commit of TodoList

---
+++ ToDoList/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ToDoList
+//
+//  Created by ZhongxingWang on Mar/20/15.
+//  Copyright (c) 2015 Alpha. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
