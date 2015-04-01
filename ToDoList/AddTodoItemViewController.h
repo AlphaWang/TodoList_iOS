efb50cac18c57f3669dcf6849de7949a7af55d75
Alpha Wang <awang@coupang.com>
2015-04-01 10:15:15 +0800

    Custom view controller `AddTodoItemViewController` for add-todo-item scene

---
+++ ToDoList/AddTodoItemViewController.h
@@ -0,0 +1,13 @@
+//
+//  AddTodoItemViewController.h
+//  ToDoList
+//
+//  Created by ZhongxingWang on Apr/1/15.
+//  Copyright (c) 2015 Alpha. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AddTodoItemViewController : UIViewController
+
+@end
---
+++ ToDoList/AddTodoItemViewController.m
@@ -0,0 +1,37 @@
+//
+//  AddTodoItemViewController.m
+//  ToDoList
+//
+//  Created by ZhongxingWang on Apr/1/15.
+//  Copyright (c) 2015 Alpha. All rights reserved.
+//
+
+#import "AddTodoItemViewController.h"
+
+@interface AddTodoItemViewController ()
+
+@end
+
+@implementation AddTodoItemViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
