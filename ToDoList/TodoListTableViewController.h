53f9fbdca5fafd3f66cea86ae3dc720c4f8b8f26
Alpha Wang <awang@coupang.com>
2015-04-01 10:18:58 +0800

    Custom view controller `TodoListTableViewController ` for todo-list scene

---
+++ ToDoList/TodoListTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  TodoListTableViewController.h
+//  ToDoList
+//
+//  Created by ZhongxingWang on Apr/1/15.
+//  Copyright (c) 2015 Alpha. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TodoListTableViewController : UITableViewController
+
+@end
